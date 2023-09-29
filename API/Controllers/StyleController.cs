aa91cb925b0e6a5eccc826e5318c49c057fff672
ndkhuong24 <kn134646@gmail.com>
2023-09-29 09:11:47 +0700

    Update

---
+++ API/Controllers/StyleController.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StyleController : ControllerBase
+    {
+        private readonly StyleDbcontext _dbContext;
+
+        public StyleController(StyleDbcontext styleDbcontext)
+        {
+            _dbContext = styleDbcontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStyle()
+        {
+            var listStyle = await _dbContext.Style.FromSqlRaw("EXEC Style_Get").ToListAsync();
+            return Ok(listStyle);
+        }
+    }
+}
---
+++ API/Data/StyleDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StyleDbcontext : DbContext
+    {
+        public StyleDbcontext(DbContextOptions<StyleDbcontext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Style>? Style { get; set; }
+    }
+}
