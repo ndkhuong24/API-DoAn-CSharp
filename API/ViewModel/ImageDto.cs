9e3a63febf99ce41616b701cde3670bdaa0aad94
ndkhuong24 <kn134646@gmail.com>
2023-10-06 10:49:34 +0700

    Update

---
+++ API/ViewModel/ImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.ViewModel
+{
+    public class ImageDto
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ API/ViewModel/ProductAndImagesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace API.ViewModel
+{
+    public class ProductAndImagesDto
+    {
+        public int CategoryID { get; set; }
+        public int BrandID { get; set; }
+        public int ProductID { get; set; }
+        public int SizeID { get; set; }
+        public int ColorID { get; set; }
+        public int SoleID { get; set; }
+        public int MaterialID { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public List<ImageDto> Images { get; set; }
+    }
+}
