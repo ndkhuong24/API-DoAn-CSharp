b08cfc241ba44f75b417920d6da165efe0b539ea
ndkhuong24 <kn134646@gmail.com>
2023-10-08 17:32:20 +0700

    update

---
+++ API/ViewModel/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace API.ViewModel
+{
+    public class OrderDTO
+    {
+        public int CustomerID { get; set; }
+        public int UserID { get; set; }
+        public int TotalPrice { get; set; }
+        public int TranSportFee { get; set; }
+        public string Description { get; set; }
+        public int VoucherID { get; set; }
+        public int DiscountPrice { get; set; }
+        public int FinalPrice { get; set; }
+        public int Status { get; set; }
+        public List<OrderDetailDTO> orderDetail { get; set; }
+    }
+}
---
+++ API/ViewModel/OrderDetailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.ViewModel
+{
+    public class OrderDetailDTO
+    {
+        public int ProductDetailId { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
