fb37508a70c6455d435167e7a31d5089ed1652cf
ndkhuong24 <kn134646@gmail.com>
2023-10-09 10:52:40 +0700

    Updare

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,22 @@
 ﻿using API.Data;
 using API.Repository;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      }); // add the allowed origins  
+});
 builder.Services.AddDbContext<StyleDbcontext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer-Connection"));
@@ -22,6 +29,8 @@ builder.Services.AddDbContext<ProductDbcontext>(options =>
 // Life cycle DI: AddSingleton(), AddTransient(), AddScoped()
 builder.Services.AddScoped<IStyleRepository, StyleRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -37,7 +46,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
+
 
 
