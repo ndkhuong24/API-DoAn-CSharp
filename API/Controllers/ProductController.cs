3a917b77646bc9429a7dc51f4e70047c8849a5fa
ndkhuong24 <kn134646@gmail.com>
2023-10-30 16:23:12 +0700

    Update-Project

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Product")]
     [ApiController]
     public class ProductController : ControllerBase
     {
