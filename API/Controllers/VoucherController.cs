607e2b2e1a00f81a11431956c99f4219a6028bae
ndkhuong24 <kn134646@gmail.com>
2023-11-18 15:54:56 +0700

    no message

---
+++ API/Controllers/VoucherController.cs
@@ -0,0 +1,65 @@
+﻿using API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace API.Controllers
+{
+    [Route("api/Voucher")]
+    [ApiController]
+    public class VoucherController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public VoucherController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("getVoucherActivity/{condition}")]
+        public async Task<IActionResult> GetVoucherActionResultAsync(int condition)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SQLServer-Connection")))
+                {
+                    await connection.OpenAsync();
+                    var command = new SqlCommand("GetVoucherActivity", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@NewCondition", condition);
+
+                    var reader = await command.ExecuteReaderAsync();
+
+                    if (await reader.ReadAsync())
+                    {
+                        var voucher = new Voucher
+                        {
+                            Id = (int)reader["id"],
+                            Code = reader["code"].ToString(),
+                            Name = reader["name"].ToString(),
+                            Type = (int)reader["type"],
+                            Value = (double)reader["value"], // Sử dụng kiểu dữ liệu double thay cho float
+                            MaximumValue = (int)reader["maximum_value"],
+                            ConditionValue = (int)reader["condition_value"],
+                            Quantity = (int)reader["quantity"],
+                            StartDate = (DateTime)reader["start_date"],
+                            EndDate = (DateTime)reader["end_date"],
+                            Status = (int)reader["status"]
+                        };
+
+                        return Ok(voucher);
+                    }
+                    else
+                    {
+                        return NotFound("Không có dữ liệu");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ API/ViewModel/Voucher.cs
@@ -0,0 +1,17 @@
+﻿namespace API.ViewModel
+{
+    public class Voucher
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public double Value { get; set; }
+        public int MaximumValue { get; set; }
+        public int ConditionValue { get; set; }
+        public int Quantity { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Status { get; set; }
+    }
+}
