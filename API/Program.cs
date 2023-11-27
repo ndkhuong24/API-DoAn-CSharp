fd9e7cf710f91c157a5b7b81b05beff9d2038908
ndkhuong24 <kn134646@gmail.com>
2023-11-27 09:26:36 +0700

    no message

--- API/Program.cs
+++ API/Program.cs
@@ -52,24 +52,24 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseCors(MyAllowSpecificOrigins);
 
-string localIP = LocalIPAddress();
-app.Urls.Add("https://" + localIP);
+//string localIP = LocalIPAddress();
+//app.Urls.Add("https://" + localIP);
 
 app.Run();
 
-static string LocalIPAddress()
-{
-    using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
-    {
-        socket.Connect("8.8.8.8", 65530);
-        IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
-        if (endPoint != null)
-        {
-            return endPoint.Address.ToString();
-        }
-        else
-        {
-            return "127.0.0.1";
-        }
-    }
-}
+//static string LocalIPAddress()
+//{
+//    using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+//    {
+//        socket.Connect("8.8.8.8", 65530);
+//        IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
+//        if (endPoint != null)
+//        {
+//            return endPoint.Address.ToString();
+//        }
+//        else
+//        {
+//            return "127.0.0.1";
+//        }
+//    }
+//}
