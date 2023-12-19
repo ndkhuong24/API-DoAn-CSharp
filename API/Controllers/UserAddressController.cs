d9fa1b3a4a9263e13262018d4057d99dd52f081c
ndkhuong24 <kn134646@gmail.com>
2023-12-19 17:33:39 +0700

    Update Project

---
+++ API/Controllers/UserAddressController.cs
@@ -0,0 +1,66 @@
+﻿using API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace API.Controllers
+{
+    [Route("api/User/Address")]
+    [ApiController]
+    public class UserAddressController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserAddressController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{userID}")]
+        public async Task<IActionResult> GetUserAddressByUserID(string userID)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SQLServer-Connection")))
+                {
+                    await connection.OpenAsync();
+                    var command = new SqlCommand("GetAddressByUserID", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@UserId", userID);
+
+                    var reader = await command.ExecuteReaderAsync();
+
+                    var results = new List<UserAddress>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        var userAddress = new UserAddress
+                        {
+                            ProvinceID = reader["ProvinceID"].ToString(),
+                            ProvinceName = reader["ProvinceName"].ToString(),
+                            DistrictID = reader["DistrictID"].ToString(),
+                            DistrictName = reader["DistrictName"].ToString(),
+                            CommuneID = reader["CommuneID"].ToString(),
+                            CommuneName = reader["CommuneName"].ToString(),
+                            DetailAddress = reader["detail_address"].ToString(),
+                            Status = reader["status"] == DBNull.Value ? 0 : (int)reader["status"]
+                        };
+
+                        results.Add(userAddress);
+                    }
+
+                    if (results.Count == 0)
+                    {
+                        return NotFound();
+                    }
+
+                    return Ok(results);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ API/ViewModel/UserAddress.cs
@@ -0,0 +1,14 @@
+﻿namespace API.ViewModel
+{
+    public class UserAddress
+    {
+        public string ProvinceID { get; set; }
+        public string ProvinceName { get; set; }
+        public string DistrictID { get; set; }
+        public string DistrictName { get; set; }
+        public string CommuneID { get; set; }
+        public string CommuneName { get; set; }
+        public string DetailAddress { get; set; }
+        public int Status { get; set; }
+    }
+}
